4d21d647cec2cb5904bdccfa770af7650c49d528
himyjan <51815522+himyjan@users.noreply.github.com>
2024-01-24 11:58:16 +0800

    upload code

--- Blazor-STYLiSH-Front-End/Blazor-STYLiSH-Front-End.Client/Api/api.cs
+++ Blazor-STYLiSH-Front-End/Blazor-STYLiSH-Front-End.Client/Api/api.cs
@@ -29,5 +29,37 @@ namespace STYLiSH.Api
         return null;
       }
     }
+
+    public async Task<MarketingCampaigns?> getCampaigns()
+    {
+      try
+      {
+        HttpResponseMessage response = await httpClient.GetAsync($"{hostname}/marketing/campaigns");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<MarketingCampaigns>();
+      }
+      catch (HttpRequestException ex)
+      {
+        // Handle the exception here
+        Console.WriteLine($"An error occurred: {ex.Message}");
+        return null;
+      }
+    }
+
+    public async Task<ProductsSearch?> searchProducts(string keyword, int paging)
+    {
+      try
+      {
+        HttpResponseMessage response = await httpClient.GetAsync($"{hostname}/products/search?keyword=${keyword}&paging=${paging}");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<ProductsSearch>();
+      }
+      catch (HttpRequestException ex)
+      {
+        // Handle the exception here
+        Console.WriteLine($"An error occurred: {ex.Message}");
+        return null;
+      }
+    }
   }
 }
